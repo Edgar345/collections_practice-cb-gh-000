5e2647a58bafbaf888c953633c31e60273d53935
Edgar V <evenegas9@ucmerced.edu>
2019-09-09 06:03:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,7 +43,7 @@ def sum_array array
 end
 
 def add_s array
-  array.each_with_index do |string, index
+  array.each_with_index do |string, index|
     if index != 1
       array[index] += "s"
     end
