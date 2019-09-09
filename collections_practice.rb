16208e82b1bdc8ff9f962089f322d2c4be22f3d4
Edgar V <evenegas9@ucmerced.edu>
2019-09-09 03:22:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,5 +7,12 @@ def sort_array_desc numbers
 end
 
 def sort_array_char_count strings_arr
-  strings_arr.sort { |a, b| a.length <=> b.length}
+  strings_arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements array, from, to
+  #modifying original array since we have a ref
+  temp = array[from]
+  array[from] = array[to]
+  array[to] = temp
 end
